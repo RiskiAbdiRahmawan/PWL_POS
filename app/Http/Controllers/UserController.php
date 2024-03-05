6af91019e30eb254c3431c25e06259e5a12f609b
RiskiAbdi Rahmawan <riskiabdi0806@gmail.com>
2024-03-05 09:26:11 +0700

    Praktikum - 1: $fillable

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,20 +11,30 @@ class UserController extends Controller
     public function index()
     {
         // Update data user dengan Eloquent model
-        $data = [
-            'nama' => 'Pelanggan Pertama'
-        ];
-        m_user::where('username', 'customer-1')->update($data);
-        // //tambah data user dengan Eloquent model
         // $data = [
-        //     'username' => 'customer-1',
-        //     'nama' => 'Pelanggan',
-        //     'password' => Hash::make('12345'),
-        //     'level_id' => '5',
+        //     'nama' => 'Pelanggan Pertama'
         // ];
-        // m_user::insert($data); //tambahkan data ke tables m_users   
-        // coba akses model UserModel
-        $user = m_user::all(); // ambil semua data dari tabel m_users
-        return view('user', ['data' => $user]);
+        // m_user::where('username', 'customer-1')->update($data);
+        // // //tambah data user dengan Eloquent model
+        // // $data = [
+        // //     'username' => 'customer-1',
+        // //     'nama' => 'Pelanggan',
+        // //     'password' => Hash::make('12345'),
+        // //     'level_id' => '5',
+        // // ];
+        // // m_user::insert($data); //tambahkan data ke tables m_users   
+        // // coba akses model UserModel
+        // $user = m_user::all(); // ambil semua data dari tabel m_users
+        // return view('user', ['data' => $user]);
+        $data = [
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345'),
+        ];
+        m_user::create($data);
+
+        $user = m_user::all();
+        return view('user',['data'=>$user]);
     }
 }
